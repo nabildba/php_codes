249e2c0258ca33e736ef7a5091461375b43110eb
naydnabil <naydnabil@gmail.com>
2021-11-26 03:03:48 +0100

    exemple gd

--- phpinfo.php
+++ phpinfo.php
@@ -4,4 +4,12 @@
 //et l'environnement du serveur
 //..
 phpinfo();
+
+//exemple de l'usage d'une extension - GD
+/*
+header ("Content-type: image/png");
+$image = imageCreate (200, 100) or die ("Erreur lors de la création de l'image");
+imagecolorallocate($image,100,90,0);
+imagepng($image);
+*/
 ?>
